699d2cad75cc27207f32d2217bfb5d5201abfd67
Marcos Bustamante Mateo <sevillamarcoss@gmail.com>
2019-11-14 03:29:46 +0100

    First version

---
+++ precache-manifest.fcfb0c7916dc365e3cbe794dde4b2be2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31ae498085cbb017e89b6df505c58395",
+    "url": "/memeGenerator/index.html"
+  },
+  {
+    "revision": "ab5dad5200ae004efd2d",
+    "url": "/memeGenerator/static/css/main.880c75d6.chunk.css"
+  },
+  {
+    "revision": "b9e5b73de1338301d372",
+    "url": "/memeGenerator/static/js/2.b16f4d33.chunk.js"
+  },
+  {
+    "revision": "ab5dad5200ae004efd2d",
+    "url": "/memeGenerator/static/js/main.7317dbad.chunk.js"
+  },
+  {
+    "revision": "3ceff5d23c80b4258149",
+    "url": "/memeGenerator/static/js/runtime-main.b8c37c0d.js"
+  },
+  {
+    "revision": "be324d5b695be0894732e4e02e8fcc9f",
+    "url": "/memeGenerator/static/media/Trollface.be324d5b.png"
+  }
+]);
